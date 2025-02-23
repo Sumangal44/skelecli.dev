57372538c3f1268376b91fd6c3db91b8f755458a
Sumangal Karan <123644743+Sumangal44@users.noreply.github.com>
2025-02-24 01:02:06 +0530

    feat: add Vercel Analytics for improved performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from '@vercel/analytics/next';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -56,7 +57,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-
+          <Analytics />
       </body>
     </html>
   );
